653c82e833590dc3cadc8a337f8445c24d3943ae
hannasong <watchns@163.com>
2018-09-03 14:23:03 +0800

    添加日志；

--- src/com/brilliance/util/HibernateUtil.java
+++ src/com/brilliance/util/HibernateUtil.java
@@ -1,14 +1,19 @@
 package com.brilliance.util;
 
+import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HibernateUtil {
 
+    static SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
     private static final SessionFactory sessionFactory = buildSessionFactory();
-
+    static Logger logger = LogUtil.getLog("HibernateUtil", "edm", format.format(new Date()));
 
     private static SessionFactory buildSessionFactory() {
         try {
